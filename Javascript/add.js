38218140817c816eccfa88c4a1b3d12f3444d990
KanchanSolanke <ksolanke@vibrenthealth.com>
2021-12-20 09:32:07 +0530

    add of two num, showing output and reverse a string

---
+++ Javascript/add.js
@@ -0,0 +1,8 @@
+const num1 = parseInt(prompt('Enter the first number '));
+const num2 = parseInt(prompt('Enter the second number '));
+
+//add two numbers
+const sum = num1 + num2;
+
+// display the sum
+document.write("num1 is"+" "+num1+"num2 is"+" "+num2+"The sum of two num is",sum);
